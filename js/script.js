3b3fdf194749bd73a4443a172618a92afd925426
Lua Specht <luaspecht@gmail.com>
2023-11-06 21:39:33 -0300

    feat: ajuste do tempo e alerta de vitoria

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
 const grid = document.querySelector('.grid')
+const spanPlayer = document.querySelector('.player')
+const timer = document.querySelector('.timer');
 
 const characters = [
   'beth',
@@ -25,7 +27,8 @@ const checkEndGame = () => {
   const disabledCards = document.querySelectorAll('.disabled-card')
 
   if (disabledCards.length === 20) {
-    alert("Você venceu! (Neste multiverso)")
+    clearInterval(this.loop);
+    alert(`Parabéns, ${spanPlayer.innerHTML} (bluurp)! Seu tempo foi de: ${timer.innerHTML} segundos!` );
   }
 }
 
@@ -112,4 +115,19 @@ const loadGame = () => {
 
 }
 
-loadGame();
+const startTimer = () => {
+
+  this.loop = setInterval(() => {
+    const currentTime = +timer.innerHTML;
+    timer.innerHTML = currentTime + 1;
+  }, 1000);
+
+}
+
+window.onload = () => {
+
+  spanPlayer.innerHTML = localStorage.getItem('player');
+  startTimer()
+  loadGame();
+}
+
